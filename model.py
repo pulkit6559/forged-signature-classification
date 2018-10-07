cf164a1de9c7f7a4962d8c2493812230904ba6e9
pulkit6559 <pulkitarora7.fas@gmail.com>
2018-10-07 21:31:01 +0530

    model

--- model.py
+++ model.py
@@ -7,4 +7,33 @@ from numpy import array
 import operator
 path = './'
 
+f_name = []
+f_array = []
+forged = []
+def create_array():
+    for (dirname, dirs, files) in os.walk('.'):
+        if dirname[2:] == "forged_bin":
+            #fig = plt.figure()
+            for filename in files:
 
+                img = Image.open(dirname+"/"+filename)
+                img = np.array(img)
+                
+                f_name.append([int(filename[9:12]), int(filename[7:9]), int(filename[4:7])])
+                f_array.append(img.ravel())
+                print(img.size)
+                forged.append(np.concatenate([f_name, f_array], axis=1))
+
+        """ if dirname[2:] == "genuine_scaled":
+            #fig = plt.figure()
+            for filename in files:
+
+                img = Image.open(dirname+"/"+filename).convert('L')
+                img = np.array(img)
+                g_name.append([int(filename[9:12]), int(filename[7:9]), int(filename[4:7])])
+                g_array.append([img]) """
+
+
+if __name__ == "__main__":
+    create_array()
+    print(forged)
