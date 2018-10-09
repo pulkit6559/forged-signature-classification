bcbed6bf4328de5974cec0a75f6d64baa15c6b1c
pulkit6559 <pulkitarora7.fas@gmail.com>
2018-10-10 01:02:30 +0530

    Normalised correlation layers

--- classifier.py
+++ classifier.py
@@ -22,3 +22,13 @@ model.add(MaxPooling2D((2, 2)))
 
 feat_map1 = model(b)
 feat_map2 = model(a)
+
+normalized_layer = Normalized_Correlation_Layer(stride=(1, 1), patch_size=(5, 5))([feat_map1, feat_map2])
+
+final_layer = Conv2D(kernel_size=(1, 1), filters=25,
+                     activation='relu')(normalized_layer)
+final_layer = Conv2D(kernel_size=(3, 3), filters=25,
+                     activation=None)(final_layer)
+final_layer = MaxPooling2D((2, 2))(final_layer)
+final_layer = Dense(500)(final_layer)
+final_layer = Dense(2, activation="softmax")(final_layer)
